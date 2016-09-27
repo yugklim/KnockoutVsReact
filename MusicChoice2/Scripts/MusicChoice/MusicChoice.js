191a749da308057357b327ae35df04000118ffeb
yugklim <yugklim@gmail.com>
2016-09-27 21:50:14 +0300

    filter works

--- MusicChoice2/Scripts/MusicChoice/MusicChoice.js
+++ MusicChoice2/Scripts/MusicChoice/MusicChoice.js
@@ -11,7 +11,25 @@
             contentType: "application/json",
             timeout: 30000,
             success: function(result) {
-                alert('OK');
+                music.filters.setState(
+                    {
+                        performers: result.Filters.Performers
+                        , genres: result.Filters.Genres
+                        //, musics: this.props.musics
+                        //, albums: this.props.albums
+                        , composers: result.Filters.Composers
+                        , casts: result.Filters.Casts
+
+                        , selected: {
+                                        performerID: result.Filters.PerformerID
+                                        , genreID: result.Filters.GenreID
+                                        //, musicID: this.props.musicID
+                                        , albumID: result.Filters.AlbumID
+                                        , composerID: result.Filters.ComposerID
+                                        , castID: result.Filters.CastID
+                                    }
+                    }
+                );
             }.bind(this),
             error: function (result) {
                 console.error(result);
