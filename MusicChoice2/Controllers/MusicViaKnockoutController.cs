048658cc032166297917bc52588316b793b67e20
yugklim <yugklim@gmail.com>
2016-10-11 21:41:39 +0300

    ko start

---
+++ MusicChoice2/Controllers/MusicViaKnockoutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicChoice2.Controllers
+{
+    public class MusicViaKnockoutController : Controller
+    {
+        //
+        // GET: /MusicViaKnockout/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
