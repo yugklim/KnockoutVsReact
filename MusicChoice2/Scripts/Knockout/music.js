af85081a13c0642e1b0891ad5b8a9ced13b7b8d0
yugklim <yugklim@gmail.com>
2016-10-12 20:55:50 +0300

    selects added via knockout

--- MusicChoice2/Scripts/Knockout/music.js
+++ MusicChoice2/Scripts/Knockout/music.js
@@ -1,7 +1,20 @@
 ﻿$(function() {
 
     var musicViewModel = {
-    
+        performers: ko.observableArray(),
+        composers: [],
+        albums: [],
+        casts: []
     };
 
+    $.getJSON("/api/MusicWebAPI", function (data) {
+        musicViewModel.performers = data.Filters.Performers;
+        musicViewModel.composers = data.Filters.Composers;
+        musicViewModel.albums = data.Filters.Albums;
+        musicViewModel.casts = data.Filters.Casts;
+
+        ko.applyBindings(musicViewModel);
+    });
+
+    
 });
