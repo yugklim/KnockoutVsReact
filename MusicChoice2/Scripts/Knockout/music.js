41ac4bc8f7e2bd2685efd93ec9f72e856a18eb42
yugklim <yugklim@gmail.com>
2016-10-14 23:51:08 +0300

    filter musics via knockout

--- MusicChoice2/Scripts/Knockout/music.js
+++ MusicChoice2/Scripts/Knockout/music.js
@@ -1,19 +1,43 @@
 ﻿$(function() {
 
     var musicViewModel = {
-        genres: [],
+        genres: ko.observableArray(),
         genresFound: ko.observableArray(),
         performers: ko.observableArray(),
-        composers: [],
-        albums: [],
-        casts: []
+        composers: ko.observableArray(),
+        albums: ko.observableArray(),
+        casts: ko.observableArray(),
+
+        genresChanged: function (data, event) {
+            $.ajax({
+                url: "/Music/FilterMusics",
+                type: "GET",
+                data: {
+                    genreIDs: musicViewModel.genresFound,
+                    "composerID": null,
+                    "albumID": null,
+                    "performerID": null,
+                    "castID": null
+                },
+                traditional: true,
+                timeout: 30000,
+                success: function (data) {
+                    musicViewModel.genres = data.Filters.Genres;
+                    musicViewModel.genresFound = data.Filters.GenresFound;
+                    musicViewModel.performers = data.Filters.Performers;
+                    musicViewModel.composers(data.Filters.Composers);
+                    musicViewModel.albums = data.Filters.Albums;
+                    musicViewModel.casts = data.Filters.Casts;
+                }
+            });
+        }
     };
 
     $.getJSON("/api/MusicWebAPI", function (data) {
         musicViewModel.genres = data.Filters.Genres;
         musicViewModel.genresFound = data.Filters.GenresFound;
         musicViewModel.performers = data.Filters.Performers;
-        musicViewModel.composers = data.Filters.Composers;
+        musicViewModel.composers(data.Filters.Composers);
         musicViewModel.albums = data.Filters.Albums;
         musicViewModel.casts = data.Filters.Casts;
 
