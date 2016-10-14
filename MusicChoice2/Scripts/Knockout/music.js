7a9cb03f0aac83d273ba334feb820805e8e4eefc
yugklim <yugklim@gmail.com>
2016-10-15 01:15:15 +0300

    filter musics via knockout

--- MusicChoice2/Scripts/Knockout/music.js
+++ MusicChoice2/Scripts/Knockout/music.js
@@ -6,43 +6,41 @@
         performers: ko.observableArray(),
         composers: ko.observableArray(),
         albums: ko.observableArray(),
-        casts: ko.observableArray(),
-
-        genresChanged: function (data, event) {
-            $.ajax({
-                url: "/Music/FilterMusics",
-                type: "GET",
-                data: {
-                    genreIDs: musicViewModel.genresFound,
-                    "composerID": null,
-                    "albumID": null,
-                    "performerID": null,
-                    "castID": null
-                },
-                traditional: true,
-                timeout: 30000,
-                success: function (data) {
-                    musicViewModel.genres = data.Filters.Genres;
-                    musicViewModel.genresFound = data.Filters.GenresFound;
-                    musicViewModel.performers = data.Filters.Performers;
-                    musicViewModel.composers(data.Filters.Composers);
-                    musicViewModel.albums = data.Filters.Albums;
-                    musicViewModel.casts = data.Filters.Casts;
-                }
-            });
-        }
+        casts: ko.observableArray()
     };
 
-    $.getJSON("/api/MusicWebAPI", function (data) {
-        musicViewModel.genres = data.Filters.Genres;
-        musicViewModel.genresFound = data.Filters.GenresFound;
-        musicViewModel.performers = data.Filters.Performers;
+    mapServerData = function(data) {
+        if (!data) return;
+        musicViewModel.genres(data.Filters.Genres);
+        musicViewModel.genresFound(data.Filters.GenresFound);
+        musicViewModel.performers( data.Filters.Performers);
         musicViewModel.composers(data.Filters.Composers);
-        musicViewModel.albums = data.Filters.Albums;
-        musicViewModel.casts = data.Filters.Casts;
+        musicViewModel.albums(data.Filters.Albums);
+        musicViewModel.casts(data.Filters.Casts);
+    },
 
+    genresChanged = function (data, event) {
+        $.ajax({
+            url: "/Music/FilterMusics",
+            type: "GET",
+            data: {
+                genreIDs: ko.toJS(musicViewModel.genresFound),
+                "composerID": null,
+                "albumID": null,
+                "performerID": null,
+                "castID": null
+            },
+            traditional: true,
+            timeout: 30000,
+            success: function (data) {
+                mapServerData(data);
+            }
+        });
+    }
+
+    $.getJSON("/api/MusicWebAPI", function (data) {
+        mapServerData(data);
         ko.applyBindings(musicViewModel);
-
     });
 
     
